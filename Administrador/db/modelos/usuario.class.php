2c952c67047a9c711f3067a653d54894e4a945ff
fearovex <32711883+fearovex@users.noreply.github.com>
2019-11-01 09:20:08 -0500

    Backups y cambios

--- Administrador/db/modelos/usuario.class.php
+++ Administrador/db/modelos/usuario.class.php
@@ -32,4 +32,28 @@
                 return false;
             }
         }
+
+        public function validateUserLicence($user) { 
+    
+            $sql = "SELECT* from usuarios a inner join licencias_usuario b on a.id = b.id_usuario
+            where a.username = '".$user."' and '".$this->getDatetimeNow()."' between b.fecha_inicial and fecha_final"; 
+    
+            $query = $this::sql($sql, Orm::FETCH_ONE);
+    
+            if(isset($query)){
+                return true;
+            }else{
+                return false;
+            }        
+        }
+    
+    
+        function getDatetimeNow() {
+            $tz_object = new DateTimeZone('America/Bogota');
+            //date_default_timezone_set('Brazil/East');
+        
+            $datetime = new DateTime();
+            $datetime->setTimezone($tz_object);
+            return $datetime->format('Y\-m\-d\ h:i:s');
+        }
     }
